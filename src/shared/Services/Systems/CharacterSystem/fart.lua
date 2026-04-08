73bbd3807d262238f174f80c1fba13e578970b62
dodman531 <102764880+dodman5311@users.noreply.github.com>
2026-04-08 14:12:12 -0400

    Not really sure

---
+++ src/shared/Services/Systems/CharacterSystem/fart.lua
@@ -0,0 +1,21 @@
+local DialogueSystem = {}
+
+--// Services
+
+--// Instances
+
+--// Modules
+
+--// Values
+
+--// Functions
+function DialogueSystem:Init()
+	--Prestart Code
+end
+
+function DialogueSystem:Start()
+	--Start Code
+end
+
+--// Main //--
+return DialogueSystem
--- src/shared/StoredData/init.lua
+++ src/shared/StoredData/init.lua
@@ -1,6 +1,7 @@
 local module = {}
 
 function module:GetData(assetName: string)
+	print(assetName)
 	local childData: ModuleScript = script:FindFirstChild(assetName)
 	if not childData then
 		return warn("no stored data by the name of " .. assetName)
